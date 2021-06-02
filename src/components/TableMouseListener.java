f50801e6eab708f2dd0e96184c744e8c74e7c446
Nguyễn Văn Hoà <1712448@student.hcmus.edu.vn>
2021-06-02 22:33:17 +0700

    feat: GiaoVuDAO, HocKiDAO, SinhVienDAO, MonHocDAO

---
+++ src/components/TableMouseListener.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class TableMouseListener extends MouseAdapter {
+    private JTable table;
+
+    public TableMouseListener(JTable table) {
+        this.table = table;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent event) {
+        // selects the row at which point the mouse is clicked
+        Point point = event.getPoint();
+        int currentRow = table.rowAtPoint(point);
+        table.setRowSelectionInterval(currentRow, currentRow);
+    }
+}
---
+++ src/hibernate/GiaovienMonHocEntity.java
@@ -0,0 +1,83 @@
+package hibernate;
+
+import java.sql.Date;
+import java.util.Objects;
+
+public class GiaovienEntity {
+    private int id;
+    private String maGiaoVien;
+    private String tenGiaoVien;
+    private String gioiTinh;
+    private Date ngaySinh;
+    private String diaChi;
+    private int idMonHoc;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMaGiaoVien() {
+        return maGiaoVien;
+    }
+
+    public void setMaGiaoVien(String maGiaoVien) {
+        this.maGiaoVien = maGiaoVien;
+    }
+
+    public String getTenGiaoVien() {
+        return tenGiaoVien;
+    }
+
+    public void setTenGiaoVien(String tenGiaoVien) {
+        this.tenGiaoVien = tenGiaoVien;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public int getIdMonHoc() {
+        return idMonHoc;
+    }
+
+    public void setIdMonHoc(int idMonHoc) {
+        this.idMonHoc = idMonHoc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GiaovienEntity that = (GiaovienEntity) o;
+        return id == that.id && idMonHoc == that.idMonHoc && Objects.equals(maGiaoVien, that.maGiaoVien) && Objects.equals(tenGiaoVien, that.tenGiaoVien) && Objects.equals(gioiTinh, that.gioiTinh) && Objects.equals(ngaySinh, that.ngaySinh) && Objects.equals(diaChi, that.diaChi);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, maGiaoVien, tenGiaoVien, gioiTinh, ngaySinh, diaChi, idMonHoc);
+    }
+}
